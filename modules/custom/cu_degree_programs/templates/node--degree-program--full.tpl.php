11f50dc9649c911c0cc0d82e0a8ac695604b7bb6
kevincrafts <kevin.crafts@colorado.edu>
2015-06-30 16:38:46 -0600

    adding if not empty for content variables - UCB-126

--- modules/custom/cu_degree_programs/templates/node--degree-program--full.tpl.php
+++ modules/custom/cu_degree_programs/templates/node--degree-program--full.tpl.php
@@ -14,15 +14,27 @@
   <section class="degree-program-sidebar">
     <?php print render($content['field_degree_prog_videos']); ?>
     <ul class="degree-program-links list-style-nobullet">
-      <li><?php print render($content['field_degree_prog_dept_website']); ?></li>
-      <li><?php print render($content['field_degree_prog_under_adm_url']); ?></li>
-      <li><?php print render($content['field_degree_prog_grad_adm_url']); ?></li>
-      <li><?php print render($content['field_degree_prog_deg_reqs_url']); ?></li>
-      <li><?php print render($content['field_degree_prog_advising_url']); ?></li>
+      <?php if (!empty($content['field_degree_prog_dept_website'])): ?>
+        <li><?php print render($content['field_degree_prog_dept_website']); ?></li>
+      <?php endif; ?>
+      <?php if (!empty($content['field_degree_prog_under_adm_url'])): ?>
+        <li><?php print render($content['field_degree_prog_under_adm_url']); ?></li>
+      <?php endif; ?>
+      <?php if (!empty($content['field_degree_prog_grad_adm_url'])): ?>
+        <li><?php print render($content['field_degree_prog_grad_adm_url']); ?></li>
+      <?php endif; ?>
+      <?php if (!empty($content['field_degree_prog_deg_reqs_url'])): ?>
+        <li><?php print render($content['field_degree_prog_deg_reqs_url']); ?></li>
+      <?php endif; ?>
+      <?php if (!empty($content['field_degree_prog_advising_url'])): ?>
+        <li><?php print render($content['field_degree_prog_advising_url']); ?></li>
+      <?php endif; ?>
     </ul>
   </section>
+  <?php if (!empty($content['field_degree_prog_additional_img'])): ?>
   <section class="degree-program-photo-gallery clearfix">
     <h3>Photos</h3>
     <?php print render($content['field_degree_prog_additional_img']); ?>
   </section>
+  <?php endif; ?>
 </article>

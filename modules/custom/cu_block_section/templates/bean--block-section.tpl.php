0b73143dc036f1edb563e2350f9650056c2fef63
Alex Finnarn <alexfinnarn@gmail.com>
2016-05-24 15:59:03 -0600

    updating geolocation code

--- modules/custom/cu_block_section/templates/bean--block-section.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="block-section-<?php print $bean->bid; ?>" class="block-section block-section-text-<?php print $content['field_hero_unit_text_color'][0]['#markup']; ?> block-section-background-<?php print $content['field_hero_unit_bg_color'][0]['#markup']; ?>" style="background-image:url(<?php print render($content['field_block_section_bg_image']); ?>);">
-  <div class="block-section-content-wrapper clearfix">
-    <div class="clearfix block-section-content block-section-content-bg-<?php print $content['field_block_section_content_bg'][0]['#markup']; ?>">
-      <?php print render($content['blocks']); ?>
-    </div>
-  </div>  
-</div>
--- modules/custom/cu_homepage_admissions/js/reset_content.js
+++ modules/custom/cu_homepage_admissions/js/reset_content.js
@@ -1,54 +1,79 @@
 (function ($) {
-  Drupal.behaviors.cu_homepage_admissions = {
-    attach: function (context, settings) {
-      // When someone clicks on one of the SHS inputs, hide the view content.
-      // This will not hide the required input text that is shown initially.
-      $(".shs-select", context).click(function () {
-        $(".view-content").addClass("element-invisible");
-        document.querySelector("#edit-field-geofield-distance-distance").setAttribute('value', '');
-        document.querySelector("#edit-field-geofield-distance-origin-lat").setAttribute('value', '');
-        document.querySelector("#edit-field-geofield-distance-origin-lon").setAttribute('value', '');
-      });
-    }
-  };
-
-  /*
-  Drupal.behaviors.cu_homepage_admissions_geolocation = {
-    attach: function (context, settings) {
-      var response, proximity_distance, long, lat, submit;
-      if (document.querySelector("#edit-field-geofield-distance-distance").getAttribute('geolocated') === 'yes') {
-        return;
-      }
-      // Get location of user.
-      httpRequest = new XMLHttpRequest();
-
-      if (!httpRequest) {
-        alert('Giving up :( Cannot create an XMLHTTP instance');
-        return false;
-      }
-      httpRequest.onreadystatechange = alertContents;
-      httpRequest.open('POST', 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD5LVNF5mw36wdisNAgI3UqNb678zLEqP4');
-      httpRequest.setRequestHeader('Content-Type', 'application/json');
-      httpRequest.send();
-
-      function alertContents() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-            response = JSON.parse(httpRequest.responseText);
-            console.log(response.location);
-            proximity_distance = document.querySelector("#edit-field-geofield-distance-distance").setAttribute('value', '100');
-            document.querySelector("#edit-field-geofield-distance-distance").setAttribute('geolocated', 'yes');
-            lat = document.querySelector("#edit-field-geofield-distance-origin-lat").setAttribute('value', response.location.lat);
-            long = document.querySelector("#edit-field-geofield-distance-origin-lon").setAttribute('value', response.location.lng);
-            //long.setAttribute('value', response.location.lng);
-            submit = document.querySelector('#edit-submit-admission-events-and-counselors').click();
-            console.log(proximity_distance, long, lat);
-          } else {
-            alert('There was a problem with the request.');
-          }
+    Drupal.behaviors.cu_homepage_admissions = {
+        attach: function (context, settings) {
+            // When someone clicks on one of the SHS inputs, hide the view content.
+            // This will not hide the required input text that is shown initially.
+            $(".shs-select", context).click(function () {
+                $(".view-content").addClass("element-invisible");
+            });
         }
-      }
-    }
-  };
-  */
+    };
+
+    Drupal.behaviors.cu_homepage_admissions_geolocation = {
+        attach: function (context, settings) {
+            var long, lat, query, httpRequest2, httpRequest, response, viewDiv;
+
+            // Don't do anything if the user has interacted with the search.
+            query = window.location.search;
+            if (query.indexOf('location') != -1) {
+                return;
+            }
+
+            //viewDiv = document.querySelector('.view-id-admission_events_and_counselors.view-display-id-block');
+            viewDiv = document.querySelector('.view-content');
+            viewDiv.innerHTML = '<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Loading...</span>';
+            //viewDiv.innerHTML = '<div style=\x22background:url(\x22://mdn.mozillademos.org/files/11987/startransparent.gif\x22) #FFEE99 2.5cm bottom no-repeat;\x22 alt=\x22Loading...\x22></div>';
+
+            /*
+            // Code for Google Geolocation API.
+            httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = locationView;
+            httpRequest.open('POST', 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD5LVNF5mw36wdisNAgI3UqNb678zLEqP4');
+            httpRequest.setRequestHeader('Content-Type', 'application/json');
+            httpRequest.send();
+            */
+
+            function locationView(position) {
+               /*
+               // Code for Google Geolocation API.
+                if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                    if (httpRequest.status === 200) {
+
+                        response = JSON.parse(httpRequest.responseText);
+                        lat = response.location.lat;
+                        long = response.location.lng;
+                        */
+
+                lat  = position.coords.latitude;
+                long = position.coords.longitude;
+
+                httpRequest2 = new XMLHttpRequest();
+                httpRequest2.onreadystatechange = locationReplace;
+                httpRequest2.open('POST', Drupal.settings.basePath + '/admissions/connect/events/ajax?long=' + long + '&lat=' + lat);
+                httpRequest2.setRequestHeader('Content-Type', 'application/json');
+                httpRequest2.send();
+                    //}
+                //}
+            }
+
+            function locationReplace() {
+                if (httpRequest2.readyState === XMLHttpRequest.DONE) {
+                    if (httpRequest2.status === 200) {
+                        response = httpRequest2.responseText;
+                        viewDiv.innerHTML = response;
+                    }
+                }
+            }
+
+            function error() {
+                console.log("Unable to retrieve your location");
+            }
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(locationView, error);
+            } else {
+                console.log("Geolocation is not supported by your browser");
+            }
+        }
+    };
 })(jQuery);

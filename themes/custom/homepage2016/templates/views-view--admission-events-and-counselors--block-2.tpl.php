b67de97c3a822f956cd3630fbfa4fa85842de514
Alex Finnarn <alexfinnarn@gmail.com>
2016-07-15 16:28:36 -0600

    updating admissions events views to include featured events

--- themes/custom/homepage2016/templates/views-view--admission-events-and-counselors--block-2.tpl.php
+++ themes/custom/homepage2016/templates/views-view--admission-events-and-counselors--block-2.tpl.php
@@ -50,21 +50,21 @@
       <?php print $attachment_before; ?>
     </div>
   <?php endif; ?>
+  <div class="admissions-geolocation-featured">
+    <?php if ($rows): ?>
+      <div class="view-content">
+        <?php print $rows; ?>
+      </div>
+    <?php elseif ($empty): ?>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print $rows; ?>
-    </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
-
+    <?php if ($pager): ?>
+      <?php print $pager; ?>
+    <?php endif; ?>
+  </div>
   <?php if ($attachment_after): ?>
     <div class="attachment attachment-after">
       <?php print $attachment_after; ?>

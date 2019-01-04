fc176e35e235184010e69f1ba3f43bb0f459e6a7
Alex Finnarn <alexfinnarn@gmail.com>
2019-01-04 11:41:26 -0500

    adding test for directory listing

--- modules/custom/cu_atoz/test/DirectoryListingTest.php
+++ modules/custom/cu_atoz/test/DirectoryListingTest.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use CU\Drupal\Directory\DirectoryListing;
+use CU\Drupal\Directory\DirectorySearch;
 use CU\Drupal\TwigHelper;
 use PHPUnit\Framework\TestCase;
 
@@ -13,13 +14,15 @@ class DirectoryListingTest extends TestCase {
   private $dl;
 
   /**
-   * @var array
+   * @var DirectorySearch
    */
-  private $data;
+  private $ds;
 
   public function setUp() {
     $this->dl = new DirectoryListing();
-    $this->data = json_decode(file_get_contents(__DIR__ . '/test_data.json'));
+
+    $data = json_decode(file_get_contents(__DIR__ . '/test_data.json'));
+    $this->ds = new DirectorySearch($data, []);
   }
 
   public function testFindOrgBySlug() {
@@ -29,6 +32,27 @@ class DirectoryListingTest extends TestCase {
       __DIR__ . '/twig_cache'
     );
 
-    $this->dl->findOrgBySlug($this->data, '', $twig_helper);
+    $result = $this->dl->findOrgBySlug($this->ds->getOrgs(), 'arts-and-sciences-college-of', $twig_helper);
+
+    $expected_result = [
+      'orgNames' => 'Arts and Sciences, College of|Academic Affairs',
+      'Type' => 'College',
+      'Listing Owner' => '',
+      'Primary Website' => '',
+      'Primary Email' => '',
+      'Staff Listing (URL)' => '',
+      'Campus Box' => '',
+      'Room' => '',
+      'Phone' => '',
+      'Campus Bird ID' => '',
+      'Contact 1 Label' => '',
+      'Contact 1 field' => '',
+      'Contact 2 Label' => '',
+      'Contact 2 Field' => '',
+      'Summary' => '',
+      'Name' => 'Arts and Sciences, College of',
+    ];
+
+    $this->assertSame($expected_result, $result);
   }
 }
